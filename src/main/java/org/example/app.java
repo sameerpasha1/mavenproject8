3dbcf5ac63ef2378d23eb69edd53f79362909d2a
sameeerpasha1 <sameeerpashas037@gmail.com>
2026-06-24 12:03:08 +0530

    message

---
+++ src/main/java/org/example/app.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class app {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 6;
+
+        // Addition
+        System.out.println("Addition: " + (a + b));
+
+        // Subtraction
+        System.out.println("Subtraction: " + (a - b));
+
+        // Multiplication
+        System.out.println("Multiplication: " + (a * b));
+
+        // Division
+        System.out.println("Division: " + (a / b));
+
+        // Modulus (Remainder)
+        System.out.println("Modulus: " + (a % b));
+    }
+}
